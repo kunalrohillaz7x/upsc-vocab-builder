55d850aeaf6a7cd4fd2ac250e0d9e9686d3675b1
kunalrohillaz7x <kunal9255r@gmail.com>
2026-06-24 16:35:16 +0530

    Connect project to Neon database

--- app/database.py
+++ app/database.py
@@ -2,7 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://postgres:1234@localhost:5432/upsc_vocab", echo=True)
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
+)
 Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
 session = SessionLocal()
